421088560e9b07260a040b0261f0a1bb2e19abf7
Greenfield01 <greenfieldadolphus22@gmail.com>
2023-07-23 13:08:23 +0100

    push

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-""" Simple heleper function."""
+"""Pagination helper function.
+"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of size two containing a start index and
-       an end index corresponding to the range of indexes to return
-       in a list for those particular pagination parameters..
+    """Retrieves the index range from a given page and page size.
     """
     start = (page - 1) * page_size
     end = start + page_size
-
     return (start, end)
-
--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-"""
-Deletion-resilient hypermedia pagination
+"""Deletion-resilient hypermedia pagination
 """
 import csv
-import math
 from typing import Dict, List
 
 
@@ -13,6 +11,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
         self.__indexed_dataset = None
 
@@ -39,24 +39,27 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """ Return a dictionary with key-value pair."""
-        assert type(index) == int and type(page_size) == int
-        assert 0 <= index < len(self.indexed_dataset())
-
-        data = []
-        next_index = index + page_size
-
-        for idx in range(index, next_index):
-            if self.indexed_dataset().get(idx):
-                data.append(self.indexed_dataset()[idx])
-            else:
-                idx += 1
-                next_index += 1
-
-        return {
-                'index': index,
-                'next_index': next_index,
-                'page_size': page_size,
-                'data': data
+        """Retrieves info about a page from a given index and with a
+        specified size.
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+        page_data = []
+        data_count = 0
+        next_index = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
+                page_data.append(item)
+                data_count += 1
+                continue
+            if data_count == page_size:
+                next_index = i
+                break
+        page_info = {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(page_data),
+            'data': page_data,
         }
-
+        return page_info
